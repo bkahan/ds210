7d36220ead87a92e700a22a79fe0b42b09e79c4f
bkahan <benkahan1@gmail.com>
2022-11-02 12:37:38 -0400

    almost done with q1 hw7

--- homework/homework7/src/main.rs
+++ homework/homework7/src/main.rs
@@ -6,6 +6,8 @@ DS210
 Collaborators: none
  */
 
+use std::f64::consts::PI;
+
 enum ShapeType {
     Rectangle,
     Circle,
@@ -13,23 +15,45 @@ enum ShapeType {
 }
 
 struct Shape {
-    sides: Vec<u16>,
-    radius: u16,
+    sides: Vec<f64>,
+    radius: f64,
     shape_type: ShapeType
 }
 
 impl Shape {
-    fn perimeter(shape: Shape) -> u16 {
-
+    fn perimeter(shape: Shape) -> f64 {
+        match shape.shape_type {
+            ShapeType::Circle => {
+                return 2.0 * shape.radius * PI ;
+            }
+            _ => {
+                let mut tmp: f64 = 0.0;
+                for side in shape.sides {
+                    tmp += &side;
+                }
+                return tmp;
+            }
+        }
     }
 
-    fn area (shape: Shape) -> u16 {
-
+    fn area (shape: Shape) -> f64 {
+        match shape.shape_type {
+            ShapeType::Circle => {
+                return f64::powf(shape.radius, 2.0) * PI ;
+            }
+            _ => {
+                let mut tmp: f64 = 0.0;
+                for side in shape.sides {
+                    tmp += &side;
+                }
+                return tmp;
+            }
+        }
     }
 
 }
 
-fn new_shape(s: Vec<u16>, r: u16, t: ShapeType) -> Shape {
+fn new_shape(s: Vec<f64>, r: f64, t: ShapeType) -> Shape {
     Shape { sides: s, radius: r, shape_type: t }
 }
 
