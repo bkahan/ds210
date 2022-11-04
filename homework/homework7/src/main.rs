c0e05096229f620795ed7c5cc0d9163f1bbc1caa
bkahan <benkahan1@gmail.com>
2022-11-03 21:48:20 -0400

    added q1 rust file

--- homework/homework7/src/main.rs
+++ homework/homework7/src/main.rs
@@ -9,8 +9,11 @@ Collaborators: none
 extern crate core;
 
 mod q1 ;
+mod q2;
 
 fn main() {
+
+    // q1
     let tri1 = vec![3.0, 4.0, 5.0];
     let mut s1 = q1::new_shape(tri1, 0.0);
 
@@ -22,5 +25,9 @@ fn main() {
     println!("{}", q1::Shape::sum_sides(&s1));
     let mut s2 = q1::new_shape(vec![1.0,2.0], 1.0); // will not let you run the code if it is not a shape
     let mut s3 = q1::new_shape(vec![-1.0,2.0,4.0], 1.0); // will not let you run the code if it is not a shape
+
+    // q2
+
+
 
 }
