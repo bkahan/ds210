b6a49a40cd585629bf8f111d06590db8e0f308bf
bkahan <benkahan1@gmail.com>
2022-11-04 18:08:56 -0400

    updated poly area, reformatted code

--- homework/homework7/src/q1.rs
+++ homework/homework7/src/q1.rs
@@ -11,66 +11,71 @@ use std::f64::consts::PI;
 pub(crate) struct Shape {
     sides: Vec<f64>,
     radius: f64,
-    is_standard_polygon: bool
+    is_standard_polygon: bool,
 }
 
-pub(crate) struct ShapeSideLen {
-    side_len: f64,
-    num_sides: u64,
-}
-
-pub(crate) trait Polygon { // question 2 implementation
-    fn new_polygon_q1(sides :Vec<f64>, radius: f64, is_standard_polygon: bool) -> Shape;
-    fn poly_area (shape : &Shape) -> f64;
-    fn poly_perimeter (shape: &Shape) -> f64;
-    fn poly_radius (shape: &Shape) -> f64;
-    fn poly_double_size (shape: &mut Shape);
-    fn sum_sides(shape :&Shape) -> f64;
-    fn new_polygon_q2(side_length: f64, num_sides: u64, is_standard_polygon: bool ) -> Shape;
+pub(crate) trait Polygon {
+    // question 2 implementation
+    fn new_polygon_q1(sides: Vec<f64>, radius: f64, is_standard_polygon: bool) -> Shape;
+    fn poly_area(shape: &Shape) -> f64;
+    fn poly_perimeter(shape: &Shape) -> f64;
+    fn poly_radius(shape: &Shape) -> f64;
+    fn poly_double_size(shape: &mut Shape);
+    fn sum_sides(shape: &Shape) -> f64;
+    fn new_polygon_q2(side_length: f64, num_sides: u64, is_standard_polygon: bool) -> Shape;
 }
 
 impl Polygon for Shape {
     fn new_polygon_q1(s: Vec<f64>, r: f64, is_standard_polygon: bool) -> Shape {
         match check_shape(&s) {
             true => Shape { sides: s, radius: r, is_standard_polygon },
-            false => Shape { sides: vec![0.0], radius : 0.0, is_standard_polygon } // return "empty" shape
+            false => Shape { sides: vec![0.0], radius: 0.0, is_standard_polygon } // return "empty" shape
         }
     }
 
-    fn new_polygon_q2(side_length: f64, num_sides: u64, is_standard_polygon: bool ) -> Shape {
+    fn new_polygon_q2(side_length: f64, num_sides: u64, is_standard_polygon: bool) -> Shape {
         if num_sides < 3 || side_length < 0.0 { // quick check for shape validity
             panic!("Not a shape.")
         }
-        let mut tmp : Vec<f64> = Vec::new() ;
+        let mut tmp: Vec<f64> = Vec::new();
         for _ in 0..=num_sides {
             tmp.push(side_length) // make a shape with n-side lengths, I know, this is bad
         }
-        return Shape::new_polygon_q1(tmp, 0.0, is_standard_polygon) ;
+        return Shape::new_polygon_q1(tmp, 0.0, is_standard_polygon);
     }
 
-    fn poly_area (shape: &Shape) -> f64 {
+    fn poly_area(shape: &Shape) -> f64 {
         if shape.radius != 0.0 {
             return f64::powf(shape.radius, 2.0) * PI;
         }
-        else {
-            match shape.sides.len() {
-                2 => f64::powf(shape.sides[0],2.0),
-                3 => {
-                    let s: f64 = Self::sum_sides(shape) / 2.0;
-                    f64::sqrt(s * (s - shape.sides[0]) * (s - shape.sides[1]) * (s - shape.sides[2]))
+        match shape.is_standard_polygon {
+            true => {
+                match shape.sides.len() {
+                    2 => return f64::powf(shape.sides[0], 2.0),
+                    3 => {
+                        let s: f64 = Self::sum_sides(shape) / 2.0;
+                        return f64::sqrt(s * (s - shape.sides[0]) * (s - shape.sides[1]) * (s - shape.sides[2]));
+                    }
+                    4 => shape.sides[0] * shape.sides[1],
+                    _ => panic!("Can't calculate anything higher than a rectangle. Use new_polygon_q2.")
                 }
-                _ => todo!() // Todo: replace with area for n-sided regular polygon
+            }
+            false => {
+                let n = shape.sides.len();
+                let s = shape.sides[0];
+                let omega = PI / n as f64;
+                let tan_omega = omega.tan();
+                n as f64 * f64::powf(s, 2.0) * (0.25) * (1.0 / tan_omega)
             }
         }
     }
 
     fn poly_perimeter(shape: &Shape) -> f64 {
-        if shape.radius != 0.0  {
-            2.0*PI*shape.radius
-        }
-        else {
+        if shape.radius != 0.0 {
+            2.0 * PI * shape.radius
+        } else {
             match shape.sides.len() {
-                2 => 4.0*shape.sides[0],  // unique case of rectangle
+                2 => 4.0 * shape.sides[0],  // unique case of rectangle
                 _ => Self::sum_sides(shape),
             }
         }
@@ -80,7 +85,7 @@ impl Polygon for Shape {
         match shape.is_standard_polygon {
             true => {
                 let s: f64 = shape.sides[0];
-                let n : f64 = shape.sides.len() as f64;
+                let n: f64 = shape.sides.len() as f64;
                 let m = 180.0 / n;
                 s / (2.0 * m.sin())
             }
@@ -90,11 +95,11 @@ impl Polygon for Shape {
 
     fn poly_double_size(shape: &mut Shape) { // defining doubling as doubling each length of side ex. [2,6,10] -> [4,12,20]
         for x in 0..shape.sides.len() {
-            shape.sides[x] = shape.sides[x]*2.0 ;
+            shape.sides[x] = shape.sides[x] * 2.0;
         }
     }
 
-    fn sum_sides(shape : &Shape) -> f64 {
+    fn sum_sides(shape: &Shape) -> f64 {
         let mut tmp: f64 = 0.0;
         for side in &shape.sides {
             tmp += side;
@@ -103,20 +108,20 @@ impl Polygon for Shape {
     }
 }
 
-fn check_shape(sides : &Vec<f64>) -> bool { // Todo: update for n-sided polygon
-    let  a:f64  = sides.iter().sum();
+fn check_shape(sides: &Vec<f64>) -> bool {
+    let a: f64 = sides.iter().sum();
     if sides.len() < 3 || a < 0.0 {
         panic!("Not a shape.")
     }
     for x in sides {
-         if *x < 0.0 {
-             panic!("Negative Side Lengths")
-         }
+        if *x < 0.0 {
+            panic!("Negative Side Lengths")
+        }
     }
     if sides.len() == 3 {
         if sides[0] + sides[1] <= sides[2] {
             panic!("Not a triangle.")
         }
     }
-    return true
+    true
 }
