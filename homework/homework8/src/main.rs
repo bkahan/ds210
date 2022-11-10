d636e1b1d0e133ef75a9cd1e5411ea34bd734e49
bkahan <benkahan1@gmail.com>
2022-11-09 22:30:38 -0500

    worked more on q1, hw8

--- homework/homework8/src/main.rs
+++ homework/homework8/src/main.rs
@@ -1,3 +1,13 @@
+/*
+Ben Kahan
+Homework 8
+DS210
+9 November 2022
+Collaborators: none
+ */
+
+mod point;
+
 fn main() {
     println!("Hello, world!");
 }
--- homework/homework8/src/point.rs
+++ homework/homework8/src/point.rs
@@ -1,1 +1,36 @@
- 
+/*
+Ben Kahan
+Homework 8
+DS210
+9 November 2022
+Collaborators: none
+ */
+
+pub mod point {
+    pub struct XYpoint<T> {
+        x : T,
+        y : T,
+    }
+
+    pub trait Point {
+        fn clockwise(&self) -> Box<Self>;
+        fn counterclockwise(&self) -> Box<Self>;
+        fn to_string(&self) -> String;
+    }
+
+    impl<T> Point for XYpoint<T> {
+        pub fn clockwise(&self) -> Box<Self> {
+            todo!()
+        }
+
+        fn counterclockwise(&self) -> Box<Self> {
+            todo!()
+        }
+
+        fn to_string(&self) -> String {
+            todo!()
+        }
+    }
+
+
+}
