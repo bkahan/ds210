de1d13b9b4600c83ccb9f7afce658d2762864a74
bkahan <benkahan1@gmail.com>
2022-11-11 18:22:24 -0500

    think q1 is done

--- homework/homework8/src/main.rs
+++ homework/homework8/src/main.rs
@@ -6,8 +6,16 @@ DS210
 Collaborators: none
  */
 
+use std::ops::Neg;
+use crate::point::point::{Point, XYpoint};
+
 mod point;
 
 fn main() {
+
+    let a: XYpoint<f64> = XYpoint { x: 0.0, y: 1.0 };
+    let b = a.neg();
+    let c = XYpoint { x:-1, y : 1};
+    let d = c.neg();
     println!("Hello, world!");
 }
--- homework/homework8/src/point.rs
+++ homework/homework8/src/point.rs
@@ -9,45 +9,43 @@ Collaborators: none
 pub mod point {
     use std::ops::Neg;
 
+    #[derive(Copy, Clone)]
     pub struct XYpoint<T> {
-        x : T,
-        y : T,
+        pub x : T,
+        pub y : T,
     }
 
-    pub trait Point: Copy + Neg {
-        fn clockwise(&self) -> Box<Self>;
-        fn counterclockwise(&self) -> Box<Self>;
-        fn to_string(&self) -> String;
+    pub trait Point {
+        fn clockwise(self) -> Self;
+        fn counterclockwise(self) -> Self;
+        fn to_string(self) -> String;
     }
 
-    impl<T> Clone for XYpoint<T> {
-        fn clone(&self) -> Self {
-            todo!()
+    impl<T:Neg<Output = T>> Neg for XYpoint<T> {
+        type Output = XYpoint<T>;
+
+        fn neg(self) -> Self {
+            return XYpoint {x: - self.x, y: - self.y};
         }
     }
 
-    impl<T> Neg for XYpoint<T> {
-        type Output = ();
+    impl Point for XYpoint<f64> {
 
-        fn neg(self) -> Self::Output {
+        fn clockwise(self) -> Self {
+            // matrix: [[0,1],[-1,0]]
+            let tmp = self::XYpoint::neg(self);
+
+            return Self { x: self.y, y : tmp.x };
+        }
+
+        fn counterclockwise(self) -> Self {
+            // matrix: [[0,-1],[1,0]]
+            return Self { x: self.y, y : self.x };
+        }
+
+        fn to_string(self) -> String {
             todo!()
         }
+
     }
-
-    impl<T> Point for XYpoint<T> {
-        fn clockwise(&self) -> Box<Self> {
-            todo!()
-        }
-
-        fn counterclockwise(&self) -> Box<Self> {
-            todo!()
-        }
-
-        fn to_string(&self) -> String {
-            todo!()
-        }
-    }
-
-
-
 }
