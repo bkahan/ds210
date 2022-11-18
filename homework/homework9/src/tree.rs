448b560570838ec84a7c36e6bee4725e184c345f
bkahan <benkahan1@gmail.com>
2022-11-18 18:44:54 -0500

    removed code, worked on error func

--- homework/homework9/src/tree.rs
+++ homework/homework9/src/tree.rs
@@ -6,11 +6,30 @@ DS210
 Collaborators: none
 */
 
-pub mod tree {
-    use std::ops::Deref;
-    use crate::readFile;
-    use std::ptr;
+/*
+take the tree
+use a determination function to split based on xi <- how do I split the data?
+get vector from that node
+push new data onto that node
+done
 
+ */
+
+/*
+build a tree based on a random split, let's say x < 0 -> 0, x > 0 -> 1
+calculate the error
+    this is determined by summing the zi for each node, dividing by length
+    we want the 0 node to be close to 0 and the 1 node close to 1
+shift the split by +/- n, recalculate error
+iterate m times, storing error each time  | Vector of trees
+return lowest error
+
+ */
+
+pub mod tree {
+    use crate::readFile;
+    use std::ops::Deref;
+    use std::ptr;
 
     struct Node {
         error: (i32, i32),
@@ -19,17 +38,15 @@ pub mod tree {
     }
 
     impl Node {
-
         fn new_tree() -> Node {
             Node {
                 error: (0, 1),
-                left_child: Vec::<(i32,i32)>::new(),
-                right_child: Vec::<(i32,i32)>::new()
+                left_child: Vec::<(i32, i32)>::new(),
+                right_child: Vec::<(i32, i32)>::new(),
             }
         }
 
-        fn init_tree(tree: Node, path : &str) {
-
+        fn init_tree(tree: Node, path: &str) {
             let mut res = readFile::read_file::file2vectuple(path);
             let data = res.unwrap();
 
@@ -39,7 +56,8 @@ pub mod tree {
             for data_point in data.iter() {
                 let x = *data_point;
 
-                if x.0 < 0 { // initialize tree to split if x < 0 -> 0, x > 0 -> 1
+                if x.0 < 0 {
+                    // initialize tree to split if x < 0 -> 0, x > 0 -> 1
                     left.push(x)
                 } else {
                     right.push(x)
@@ -47,37 +65,34 @@ pub mod tree {
             }
         }
 
-        fn insert_node(tree: Node, data : Vec<(i32, i32)>) {
+        fn recalculate_tree(tree: Node) {
 
-            /*
-            take the tree
-            use a determination function to split based on xi <- how do I split the data?
-            get vector from that node
-            push new data onto that node
-            done
-
-             */
-
-
-
-            todo!()
+            
         }
 
-        fn determine(data: (i32, i32)) -> bool {
+        fn calculate_error(tree: Node) -> (i32, i32) {
+            // return error
 
+            let left = tree.left_child; // try to get to 0
+            let right = tree.right_child; // try to get to 1
 
-            /*
-            build a tree based on a random split, let's say x < 0 -> 0, x > 0 -> 1
-            calculate the error
-                this is determined by summing the zi for each node, dividing by length
-                we want the 0 node to be close to 0 and the 1 node close to 1
-            shift the split by +/- n, recalculate error
-            iterate m times, storing error each time  | Vector of trees
-            return lowest error
+            let mut left_sum = 0;
+            let mut right_sum = 0;
 
-             */
+            for data_point in left.iter() {
+                let tmp = *data_point;
+                left_sum = left_sum + tmp.1;
+            }
 
-            todo!()
+            for data_point in right.iter() {
+                let tmp = *data_point;
+                right_sum = right_sum + tmp.1;
+            }
+
+            let left_err = 0 - (left_sum / left.len() as i32);
+            let right_err = 1 - (right_sum / left.len() as i32);
+
+            return (left_err, right_err);
         }
     }
 }
