49b14a9a23417cb2cf82815c94a8ae1912e4dd21
bkahan <benkahan1@gmail.com>
2022-11-18 00:20:57 -0500

    finished parsing file

--- homework/homework9/src/main.rs
+++ homework/homework9/src/main.rs
@@ -15,20 +15,6 @@ mod tree;
 
 fn main() {
     let path = "/Users/benkahan/Documents/School/ds210/homework/homework9/src/pagerank_data.txt";
-    let data = fs::read(path).expect("Unable to read file");
 
-    fn read_file_vec(filepath: &str) -> Result<Vec<(u32, u32)>, Box<dyn std::error::Error>> {
-        let data = File::open(filepath)?;
-        let reader = BufReader::new(data);
-
-        let tmp_res: Vec<(u32, u32)> = Vec::new();
-
-        for line in reader.lines() {
-            println!("{}", line.unwrap())
-        }
-
-        todo!()
-    }
-
-    let res = read_file_vec(path);
+    let res = readFile::read_file::file2vectuple(path);
 }
--- homework/homework9/src/readFile.rs
+++ homework/homework9/src/readFile.rs
@@ -8,8 +8,26 @@ Collaborators: none
 
 use std::fs;
 
-
 pub mod read_file {
+    use std::fs::File;
+    use std::io::{BufRead, BufReader};
 
+    pub fn file2vectuple(filepath: &str) -> Result<Vec<(u32, u32)>, Box<dyn std::error::Error>> {
+        let data = File::open(filepath)?;
+        let reader = BufReader::new(data);
 
+        let mut tmp_res: Vec<(u32, u32)> = Vec::new();
+
+        for line in reader.lines() {
+            let tmp = line.unwrap();
+            let mut split_lines = tmp.split(' ');
+            let res = (
+                split_lines.next().unwrap().parse::<u32>().unwrap(),
+                split_lines.next().unwrap().parse::<u32>().unwrap(),
+            );
+            tmp_res.push(res);
+        }
+
+        Ok(tmp_res)
+    }
 }
