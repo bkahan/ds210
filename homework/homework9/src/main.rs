4fa57fda9671cd515501da8b6bbda6b8b62daa9a
bkahan <benkahan1@gmail.com>
2022-11-18 21:46:13 -0500

    finished hw9, reformatted code

--- homework/homework9/src/main.rs
+++ homework/homework9/src/main.rs
@@ -6,46 +6,22 @@ DS210
 Collaborators: none
 */
 
-use std::fs;
-use std::fs::File;
-use std::io::{BufRead, BufReader};
-use crate::tree::tree::Node;
-
-mod readFile;
+mod read_file;
 mod tree;
-
 
 fn main() {
     let path = "/Users/benkahan/Documents/School/ds210/homework/homework9/src/data.txt"; // I needed to use the absolute path for my computer
     //let path = "data.txt";
 
-    let mut treee = tree::tree::Node::new_tree() ;
+    let mut treee = tree::tree::Node::new_tree();
     tree::tree::Node::init_tree(&mut treee, path);
 
-    let mut tree_vec = tree::tree::Node::iterate_tree(&mut treee, 50);
+    let mut tree_vec = tree::tree::Node::iterate_tree(&mut treee, 75);
+    let mut tree_params = tree::tree::Node::find_best_tree_param(&mut tree_vec);
 
-    let init_node = tree_vec.pop().unwrap();
-    let mut error = tree::tree::Node::get_error(&init_node);
-
-    let mut lowest = Vec::<(f32,f32)>::new();
-    let mut final_tree = Vec::<&Node>::new();
-
-    for tree in tree_vec.iter() {
-        let curr_tree_err = tree::tree::Node::get_error(tree);
-
-         if curr_tree_err < error  {
-             error = curr_tree_err ;
-             lowest.push(error);
-             final_tree.push(tree);
-         }
-    }
-
-    let final_tree = final_tree.pop().unwrap();
-    let split_point = tree::tree::Node::get_split_point(final_tree);
-    println!("{}", split_point);
-
-
-
-
-
+    println!("If x < {}, then: ", tree_params.2);
+    println!("Output:");
+    println!("     output 0");
+    println!("Else: ");
+    println!("     output 1");
 }
--- homework/homework9/src/tree.rs
+++ homework/homework9/src/tree.rs
@@ -13,9 +13,6 @@ get vector from that node
 push new data onto that node
 done
 
- */
-
-/*
 build a tree based on a random split, let's say x < 0 -> 0, x > 0 -> 1
 calculate the error
     this is determined by summing the zi for each node, dividing by length
@@ -27,40 +24,33 @@ return lowest error
  */
 
 pub mod tree {
-    use crate::readFile;
+    use crate::read_file;
     use crate::tree::tree;
 
     pub struct Node {
         error: (f32, f32),
         split_point: i32,
-        left_child: Vec<(i32, i32)>, // todo fix this
+        left_child: Vec<(i32, i32)>,
         right_child: Vec<(i32, i32)>,
     }
 
     impl Node {
-
         pub fn get_error(node: &Node) -> (f32, f32) {
             return node.error;
-        }
-
-        pub fn get_split_point(node : &Node) -> i32 {
-            return node.split_point;
         }
 
         pub fn new_tree() -> Node {
             Node {
                 error: (0.0, 1.0),
-                split_point : 0,
+                split_point: 0,
                 left_child: Vec::<(i32, i32)>::new(),
                 right_child: Vec::<(i32, i32)>::new(),
             }
         }
 
-        fn insert(tree: &mut Node, data : &Vec<(i32, i32)>, split_point : i32) {
-
-            let mut left  = &mut tree.left_child;
-            let mut right = &mut tree.right_child;
-
+        fn insert(tree: &mut Node, data: &Vec<(i32, i32)>, split_point: i32) {
+            let left = &mut tree.left_child;
+            let right = &mut tree.right_child;
 
             for data_point in data.iter() {
                 let x = *data_point;
@@ -75,10 +65,10 @@ pub mod tree {
         }
 
         pub fn init_tree(tree: &mut Node, path: &str) {
-            let res = readFile::read_file::file2vectuple(path);
+            let res = read_file::read_file::file2vectuple(path);
             let data = res.unwrap();
 
-            tree::Node::insert(tree,&data, tree.split_point);
+            tree::Node::insert(tree, &data, tree.split_point);
             tree::Node::calculate_error(tree);
         }
 
@@ -88,8 +78,8 @@ pub mod tree {
             let left = &tree.left_child; // try to get to 0
             let right = &tree.right_child; // try to get to 1
 
-            let mut left_sum :f32 = 0.0;
-            let mut right_sum : f32 = 0.0;
+            let mut left_sum: f32 = 0.0;
+            let mut right_sum: f32 = 0.0;
 
             for data_point in left.iter() {
                 let tmp = *data_point;
@@ -104,13 +94,12 @@ pub mod tree {
             let left_err = 1.0 - (left_sum / left.len() as f32);
             let right_err = 1.0 - (right_sum / left.len() as f32);
 
-            tree.error = (left_err, right_err) ;
+            tree.error = (left_err, right_err);
 
             return (left_err, right_err);
         }
 
-        pub fn iterate_tree(tree : &mut Node, num_iters : i32) -> Vec<Node> {
-
+        pub fn iterate_tree(tree: &mut Node, num_iters: i32) -> Vec<Node> {
             let mut data = Vec::<(i32, i32)>::new();
             let mut result = Vec::<Node>::new();
 
@@ -130,16 +119,36 @@ pub mod tree {
                 result.push(tmp_tree)
             }
 
-            for iter in 0..num_iters { // https://stackoverflow.com/questions/69170796/rust-negative-for-loops
+            for iter in 0..num_iters {
+                // https://stackoverflow.com/questions/69170796/rust-negative-for-loops
                 let mut tmp_tree = tree::Node::new_tree();
-                tree::Node::insert(&mut tmp_tree, &data, - iter);
+                tree::Node::insert(&mut tmp_tree, &data, -iter);
                 tree::Node::calculate_error(&mut tmp_tree);
                 tmp_tree.split_point = iter;
                 result.push(tmp_tree)
             }
 
             return result;
+        }
+        pub fn find_best_tree_param(tree_vec: &mut Vec<Node>) -> (f32, f32, i32) {
+            let init_node = tree_vec.pop().unwrap();
+            let mut error = tree::tree::Node::get_error(&init_node);
 
+            let mut lowest = Vec::<(f32, f32)>::new();
+            let mut final_tree = Vec::<&Node>::new();
+
+            for tree in tree_vec.iter() {
+                let curr_tree_err = tree::tree::Node::get_error(tree);
+
+                if curr_tree_err < error {
+                    error = curr_tree_err;
+                    lowest.push(error);
+                    final_tree.push(tree);
+                }
+            }
+            let tmp = final_tree.pop().unwrap();
+
+            return (tmp.error.0, tmp.error.1, tmp.split_point);
         }
     }
 }
