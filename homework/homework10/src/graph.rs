3521258f5ad735ff757713935acee80361d81a9a
bkahan <benkahan1@gmail.com>
2022-11-21 17:12:18 -0500

    outlined the rest of hw10

--- homework/homework10/src/graph.rs
+++ homework/homework10/src/graph.rs
@@ -34,22 +34,14 @@ pub mod graph {
             return Graph { vert_count: num_vertex, adj_list: list_tmp, adj_matrix: mat_tmp };
         }
 
-        pub fn insert_dedge(dedge: &Vec<(usize, usize)>, mut graph: Graph) {
-
-            for (_source, _target) in dedge {
+        pub fn insert_data(data: &Vec<(usize, usize)>, mut graph: Graph) {
+            for (_source, _target) in data {
                 graph.adj_list[*_source].push(*_target);
                 graph.adj_matrix[*_source].push(true);
-
             }
-
-            /*
-            takes in a tuple (edge1, edge2) and a vertex
-            inserts by going to index of edge1 and distance to edge2
-            return (optional) true <- look into this
-             */
         }
 
-        pub fn traverse() {
+        pub fn traverse() { // todo: not actually sure if I need this
             todo!()
 
             /*
@@ -58,8 +50,72 @@ pub mod graph {
              */
         }
 
-        pub fn pagerank() {
+        pub fn pagerank(graph : Graph ) {
             todo!()
+
+            /*
+                Without going into details of how exactly PageRank is defined, we can compute its
+                approximation by simulating 100 random independent walks from each vertex. Each
+                random walk should consist of 100 random steps as described above. Our
+                approximation of PageRank for a vertex v is then the fraction of the random walks that
+                terminated at v (i.e., the number of random walks that terminated at v divided by 100n,
+                where n is the number of vertices). Notice that the sum of PageRank across all vertices
+                should add to 1.0 (modulo any precision errors in floating point arithmetic).
+
+                If v has no outgoing edges, jump to a uniformly random vertex in the entire
+                graph
+                If v has at least one outgoing edge:
+                    With probability 9/10, select uniformly at random one of them and follow it. <- use rand::distributions::Bernoulli to do this
+                    Otherwise—i.e., with probability 1/10—jump to a vertex selected uniformly at random from the entire graph
+
+
+                pagerank of vertex v := (# walks that terminate at v) / 100 * (num of v)
+                check and verify: sum of (pagerank of v_i) = 1 <- test function
+
+                vertex_data_structs:
+                - vertex id : float
+                - page_rank : float
+                - times visited: int
+
+                page_rank_traverse(graph) returns vector of vertex_data_structs
+
+                set res : vector<vertex_data_structs> => new vector
+
+                set
+
+                for vec in vector in vector<vertex>:
+                    push new vertex_data_struct with:
+                        vertex id = vec id
+                        page rank = 0
+                        times visited = 0
+
+                for _ in 0..100:
+                    for v in vertex in graph:
+                        set m : vec<vertex> to be the immediate neighbors of v
+                        if m is empty:
+                            randomly pick vertex in graph
+                        if m.len() >= 1:
+                            with prob = .9 => randomly pick vertex w from m
+                                                get id of m, index res[id of m], increment times visited
+                            otherwise => randomly pick vertex q in graph
+                                                get id of q, index res[id of q], increment times visited
+
+
+                page_rank_calculate(vertex_data_structs):
+                    set s to # of nodes in graph
+                    for ver in vertex_data_structs:
+                        set ver.page_rank to (ver.times visited / s ) : float
+
+
+
+
+
+
+
+
+
+
+             */
 
             /*
             todo: not sure what to do here
--- homework/homework10/src/read_file.rs
+++ homework/homework10/src/read_file.rs
@@ -10,7 +10,7 @@ pub(crate) mod read_file {
     use std::fs::File;
     use std::io::{BufRead, BufReader};
 
-    pub fn file2vectuple(filepath: &str) -> Result<Vec<(usize, usize)>, Box<dyn std::error::Error>> {
+    pub fn file2vectuple(filepath: &str) -> Result<Vec<(usize, usize)>, Box<dyn std::error::Error>> { // adapted from my hw9
         let data = File::open(filepath)?;
         let reader = BufReader::new(data);
 
@@ -18,16 +18,19 @@ pub(crate) mod read_file {
 
         for line in reader.lines() {
             let tmp = line.unwrap();
-            let mut split_lines: Vec<&str> = tmp.split(' ').collect();
+            let split_lines: Vec<&str> = tmp.split(' ').collect();
 
             let mut res: (usize, usize) = (0, 0);
 
-            if split_lines.len() == 1  { // make the first node be the dimensions todo: make this better, this is shit code
-                res.0 = split_lines[0].parse().unwrap();
-                res.1 = res.0;
-            } else {
-                res.0 = split_lines[0].parse().unwrap();
-                res.1 = split_lines[1].parse().unwrap();
+            match split_lines.len() { // todo: still kinda shit but better
+                1 => {
+                    res.0 = split_lines[0].parse().unwrap();
+                    res.1 = res.0;
+                },
+                _ => {
+                    res.0 = split_lines[0].parse().unwrap();
+                    res.1 = split_lines[1].parse().unwrap();
+                }
             }
             tmp_res.push(res);
         }
